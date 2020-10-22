be56015c6d18afa0fa0828ea00f00e283f375cc3
Ahmet Yesil <yesil.ahmet@ing.com>
2020-10-22 12:38:25 +0200

    fix: normalize webkit date formatting without year

--- packages/localize/src/date/normalizeIntlDate.js
+++ packages/localize/src/date/normalizeIntlDate.js
@@ -2,9 +2,10 @@
  * To filter out some added characters in IE
  *
  * @param {string} str
+ * @param {import('@lion/localize/types/LocalizeMixinTypes').FormatDateOptions} [options] Intl options are available
  * @returns {string}
  */
-export function normalizeIntlDate(str) {
+export function normalizeIntlDate(str, { weekday, year, month, day } = {}) {
   const dateString = [];
   for (let i = 0, n = str.length; i < n; i += 1) {
     // remove unicode 160
@@ -18,5 +19,19 @@ export function normalizeIntlDate(str) {
     }
   }
 
-  return dateString.join('');
+  const result = dateString.join('');
+
+  // Normalize webkit date formatting without year, in this case,
+  // only webkit is missing the comma in the date "Saturday, 12 October"
+  if (
+    !year &&
+    weekday === 'long' &&
+    month === 'long' &&
+    day === '2-digit' &&
+    result.indexOf(',') === -1
+  ) {
+    return result.replace(' ', ', ');
+  }
+
+  return result;
 }
